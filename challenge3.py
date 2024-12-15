67154d89f349f9e3d41965e7330a8709a1f4d66d
Guilherme Casagrande <casagrande.gui@gmail.com>
2024-12-14 21:34:18 -0300

    Create challenge3.py

---
+++ challenge3.py
@@ -0,0 +1,6 @@
+# 3. Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a subtração do primeiro pelo o segundo valor.
+
+numero1 = int(input('Digite um número de 1 a 10: '))
+numero2 = int(input('Digite outro número de 1 a 10: '))
+subt = numero1 - numero2
+print('A diferença entre os números escolhidos é',subt)
