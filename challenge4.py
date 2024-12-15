db451779a5e33ff46c042b0facc41176d4734cc9
Guilherme Casagrande <casagrande.gui@gmail.com>
2024-12-14 21:34:56 -0300

    Create challenge4.py

---
+++ challenge4.py
@@ -0,0 +1,6 @@
+# 4. Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a multiplicação dos dois valores.
+
+numero1 = int(input('Digite um número de 1 a 10: '))
+numero2 = int(input('Digite outro número de 1 a 10: '))
+multi = numero1 * numero2
+print('Se multiplicarmos esses dois números teremos o resultado',multi)
