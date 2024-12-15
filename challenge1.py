d3664975a6f89d83f8cc1531755cbcae9222d5b2
Guilherme Casagrande <casagrande.gui@gmail.com>
2024-12-14 21:32:14 -0300

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,6 @@
+# 1) Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a soma dos dois valores.
+
+numero1 = int(input('Digite um número de 1 a 10: '))
+numero2 = int(input('Digite outro número de 1 a 10: '))
+soma = numero1 + numero2
+print('A soma dos números escolhidos é',soma)
