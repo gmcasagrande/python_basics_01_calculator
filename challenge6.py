11931eca0b726c5f250dc3839b65bd547f00f7d7
Guilherme Casagrande <casagrande.gui@gmail.com>
2024-12-14 21:36:54 -0300

    Create challenge6.py

---
+++ challenge6.py
@@ -0,0 +1,6 @@
+# 6. Crie um programa que solicite dois valores numéricos, um operador e uma potência, e realize a exponenciação entre esses dois valores.
+
+numero1 = int(input('Digite um número de 1 a 10: '))
+numero2 = int(input('Digite outro número de 1 a 10: '))
+potencia = numero1 ** numero2
+print('Se elevarmos o primeiro número à potência do segundo número teremos o resultado ',potencia)
