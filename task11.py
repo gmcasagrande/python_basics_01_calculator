40239645a960d37e35b108a6ac0c24339368a3e6
Guilherme Casagrande <casagrande.gui@gmail.com>
2025-04-24 22:49:05 -0300

    Create task11.py

---
+++ task11.py
@@ -0,0 +1,4 @@
+import math
+print('Plantas Círculo da Vida')
+raio = int(input('informe o valor do raio da área circular: '))
+print(f'O valor do seu jardim será R${(25 * (math.pi*(pow(raio,2)))):.2f}')
